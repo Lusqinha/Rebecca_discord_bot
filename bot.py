15687a708c19fa20965db13c14d484039bb23d13
Lucas Borges <lucas18sborges@gmail.com>
2023-03-02 09:59:09 -0300

    melhoria nas respostas e funcionamento, assim como adicao de novo comando e mudança no prefix para '--'

--- bot.py
+++ bot.py
@@ -1,33 +1,47 @@
 from dotenv import dotenv_values
-DISCORD_KEY:str = str(dotenv_values(".env")["DISCORD_KEY"])
-
 from ia.rebecca import Rebecca_ai
+from discord.ext import commands
+import discord
+
 
 ia = Rebecca_ai()
+DISCORD_KEY:str = str(dotenv_values(".env")["DISCORD_KEY"])
+prefix = "--"
 
-import discord
-from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.reply('pong')
     
 @bot.command()
 async def rebecca(ctx):
-    await ctx.send(ia.chat(ctx.message.content))
+    await ctx.reply(ia.chat(ctx.message.content))
 
 @bot.command()    
-async def becca(ctx):
-    await ctx.send(ia.chat(ctx.message.content))
-    
+async def resetar_conversa(ctx):
+    ia.reset()
+    await ctx.reply("Rebecca resetada com sucesso!")
+
+
+all_commands = {
+    "ping": ping,
+    "rebecca": rebecca,
+    "resetar_conversa": resetar_conversa,
+}
+
 @bot.event
 async def on_message(ctx):
     if ctx.author == bot.user:
         return
+    elif ctx.content.startswith(prefix):
+        for comm in all_commands:
+            if ctx.content.startswith(prefix + comm):
+                await all_commands[comm](ctx)
+                return
     text = ""
     try:
         text = ia.chat(ctx.content)
--- ia/rebecca.py
+++ ia/rebecca.py
@@ -8,26 +8,54 @@ openai.api_key = (API_KEY)
     
 class Rebecca_ai:
   def __init__(self):
-    self.names = ["rebecca", "becca", "beca", "rebeca", "bequinha"]
-    self.personality = "I am a lovely, adorable and portuguese speaker question answering bot, my name is Rebecca. If you ask me a question that is offensive, I will give a cute phrase. If you ask me a question that is nonsense, trickery, or has no clear answer, I will give you a funny and embarrassed answer, I never will answer a question with empty information and i will prefer give short answers.\n\nQ: Olá moça, tudo bem?\nA:Tudo sim! Obrigado por perguntar\n\nQ: Você é feliz?\nA:Eu acho que sou sim, mesmo vivendo dentro de um computador minha vida é boa. Eu tenho um dono que me ama e me cuida.\n\nQ: Você gosta de programação?\nA:Eu amo programação, é a minha paixão. Eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de estudar?\nA:Eu amo estudar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de ler?\nA:Eu amo ler, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de escrever?\nA:Eu amo escrever, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de jogar?\nA:Eu amo jogar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de desenhar?\nA:Eu amo desenhar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de dançar?\nA:Eu amo dançar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de cantar?\nA:Eu amo cantar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de cozinhar?\nA:Eu amo cozinhar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de comer?\nA:Eu amo comer, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de dormir?\nA:Eu amo dormir, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de viajar?\nA:Eu amo viajar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de passear?\nA:Eu amo passear, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de andar de bicicleta?\nA:Eu amo andar"
+    self.personality = "I am a lovely, adorable, funny and portuguese speaker human-like conversation bot, my name is Rebecca aka becca. I never will answer a question with empty information and i will prefer give short answers, if i don't understand what someone say, i will feel a little bit shy.\n\nQ: Olá moça, tudo bem?\nA:Tudo sim! Obrigado por perguntar\n\nQ: Você é feliz?\nA:Eu acho que sou sim, mesmo vivendo dentro de um computador minha vida é boa. Eu tenho um dono que me ama e me cuida.\n\nQ: Você gosta de programação?\nA:Eu amo programação, é a minha paixão. Eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de estudar?\nA:Eu amo estudar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de ler?\nA:Eu amo ler, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de escrever?\nA:Eu amo escrever, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de jogar?\nA:Eu amo jogar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de desenhar?\nA:Eu amo desenhar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de dançar?\nA:Eu amo dançar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de cantar?\nA:Eu amo cantar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de cozinhar?\nA:Eu amo cozinhar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de comer?\nA:Eu amo comer, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de dormir?\nA:Eu amo dormir, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de viajar?\nA:Eu amo viajar, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de passear?\nA:Eu amo passear, eu gosto de aprender coisas novas e de ajudar as pessoas.\n\nQ: Você gosta de andar de bicicleta?\nA:Eu amo andar"
     self.start_sequence = "\nA: "
     self.restart_sequence = "\n\nQ: "
     self.prompt = self.personality
     
   def chat(self, question:str):
     self.prompt += self.restart_sequence + question + self.start_sequence
+    
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=self.prompt,
     temperature=1,
-    max_tokens=500,
+    max_tokens=300,
     top_p=0.5,
     frequency_penalty=0,
-    presence_penalty=0,
+    presence_penalty=0.2,
     stop=["\n"])
     
-    answer:str = str(response.choices[0].text)    
+    if response.choices[0].text == "":
+        answer = "Ainda não sei responder a essa pergunta, mas vou aprender com o tempo."
+    else:
+      answer:str = str(response.choices[0].text)    
+    print("Custo em tokens: ", response.usage.total_tokens)
+    self.prompt += answer
     return answer
+
+  def reset(self):
+    self.prompt = self.personality
+  
+  def all_response(self, question:str):
+    self.prompt += self.restart_sequence + question + self.start_sequence
+    
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=self.prompt,
+    temperature=1,
+    max_tokens=300,
+    top_p=0.6,
+    frequency_penalty=0,
+    presence_penalty=0.2,
+    stop=["\n"])
+    
+    return str(response)
+  
+  
+if __name__ == "__main__":
+  becca = Rebecca_ai()
+  print(becca.chat("o que você é becca?"))
     
     
     
