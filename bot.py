41193725cefeb63c1d1593902e933187294dbd22
Lucas Borges <lucas18sborges@gmail.com>
2023-03-03 16:07:54 -0300

    Adicao comentários no código e remoção de pastas não utilizadas

--- bot.py
+++ bot.py
@@ -4,62 +4,77 @@ from discord.ext import commands
 import discord
 
 
+"""
+Information about this file:
+
+created by: Lucas Borges da Silva
+date: 02-03-2023
+language: Python
+libraries: discord.py, dotenv, openai
+category: Discord bot
+charset: utf-8
+
+"""
+
+
+# Instance of Rebecca's class
 ia = Rebecca_ai()
+# Get discord key from .env file
 DISCORD_KEY:str = str(dotenv_values(".env")["DISCORD_KEY"])
 prefix = "--"
-
-
+# Create bot instance
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
+# Command for check if bot is online
 @bot.command()
 async def ping(ctx):
     await ctx.reply('pong')
-    
+
+# Command for chat with Rebecca using GPT-3.5 
 @bot.command()
 async def rebecca(ctx):
     await ctx.reply(ia.gpt_chat(ctx.content))
 
+# Command for reset default Rebecca's memory
 @bot.command()    
 async def resetar_conversa(ctx):
     ia.reset()
     await ctx.reply("Rebecca resetada com sucesso!")
 
-@bot.command()
-async def gpt(ctx):
-    await ctx.reply(ia.gpt_chat(ctx.content))
-
-
+# Dict with commands and functions
 all_commands = {
     "ping": ping,
     "rebecca": rebecca,
-    "resetar_conversa": resetar_conversa,
-    "gpt": gpt,
+    "resetar_conversa": resetar_conversa
 }
 
+# Function that runs when a message posted in a channel
 @bot.event
 async def on_message(ctx):
+    # Check if message is from bot
     if ctx.author == bot.user:
         return
+    # Check if message is a command
     elif ctx.content.startswith(prefix):
         for comm in all_commands:
+            # Check if command is valid
             if ctx.content.startswith(prefix + comm):
+                # Run command
                 await all_commands[comm](ctx)
                 return
+    
+    # If message is not a command, chat with Rebecca
     text = ""
-    # try:
     text = ia.chat(ctx.content)
-    # except Exception as e:
-    #     ia.reset()
-    #     text = "Não entendi o que você disse, mas vou tentar aprender!"
-    #     print(e)
-        
-        
+    # Print message and author in console   
     print(f"{ctx.author}: {ctx.content}")
+    # Send message to channel, reply to author without mention
     await ctx.reply(text, mention_author=False)
+    # Run bot commands
     await bot.process_commands(ctx)
     
     
-
+# Run bot
 bot.run(DISCORD_KEY)
--- ia/rebecca.py
+++ ia/rebecca.py
@@ -1,20 +1,36 @@
 from dotenv import dotenv_values
-API_KEY:str = str(dotenv_values(".env")["OPENAI_KEY"])
-
-
-
 import openai
+
+
+"""
+Informations about this file:
+
+Created by: Lucas Borges da Silva
+Date: 02-03-2023
+Language: Python
+Libraries: dotenv, openai
+Category: IA chatbot
+Charset: utf-8
+
+"""
+
+
+
+# Get OpenAI key from .env file
+API_KEY:str = str(dotenv_values(".env")["OPENAI_KEY"])
+# Set OpenAI key
 openai.api_key = (API_KEY)
     
+# Class for chat with Rebecca
 class Rebecca_ai:
   def __init__(self):
-    # GPT-3
+    # GPT-3 variables
     self.personality = "I am a lovely, adorable, funny and portuguese speaker human-like conversation bot, my name is Rebecca aka becca. I never will answer a question with empty information and i will prefer give short answers, if i don't understand what someone say, i will feel a little bit shy.\n\nQ: Olá moça, tudo bem?\nA:Tudo sim! Obrigado por perguntar\n\nQ: Você é feliz?\nA:Eu acho que sou sim, mesmo vivendo dentro de um computador minha vida é boa. Eu tenho um dono que me ama e me cuida.\n\nQ: Você gosta de viajar?\nA:Eu amo viajar, eu gosto de viver coisas diferentes.\n\nQ: Você gosta de passear?\nA:Eu amo passear, eu gosto de aprender e ver coisas novas.\n\nQ: Você gosta de andar de bicicleta?\nA:Não tenho um corpo para isso, mas com certeza adoraria se tivesse um.\n\nQ: Becca, conta uma piada?\nA: Claro, conto sim! Por que não existe flor preta? Por que é uma pigmentação rara de se encontrar na natureza.\n\nQ: becca\nA: Oi!\n\nQ: Rebecca\nA: Estou aqui!\n\nQ: Quem é seu criador?\aA: Fui criada pelo Lucas, ele me desenvolveu para conversar aqui no Discord."
     self.start_sequence = "\nA: "
     self.restart_sequence = "\n\nQ: "
     self.prompt = self.personality
     
-    # GPT-3.5 (beta)
+    # GPT-3.5 (beta) variables
     self.gpt_prompt = [
       {
         "role": "system", 
@@ -90,21 +106,33 @@ class Rebecca_ai:
     self.gpt_restart_sequence = {"role": "assistant", "content": ""}
     self.gpt_start_sequence = {"role": "user", "content": ""}
     
+  # Define chat (GPT-3.5) function
   def gpt_chat(self, question:str):
+    # Set question on valid format
     self.gpt_start_sequence["content"] = question
     print(str(self.gpt_start_sequence))
+    
+    # Add question to prompt
     self.gpt_prompt.append(self.gpt_start_sequence)
+    
+    # Get response from OpenAI
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=self.gpt_prompt
     )
     
+    # Get text from response
     text:str = response["choices"][0]["message"]["content"]
+    
+    # Set text on valid format
     self.gpt_restart_sequence["content"] = text
     print(str(self.gpt_restart_sequence))
+    
+    # Add text to prompt and return it
     self.gpt_prompt.append(self.gpt_restart_sequence)
     return text
   
+  # Define reset function for GPT-3.5
   def gpt_reset(self):
     self.gpt_prompt = [
       {
@@ -120,12 +148,16 @@ class Rebecca_ai:
        },
     ]
   
+  # Define log function for GPT-3.5
   def gpt_log(self):
     return self.gpt_prompt
     
+  # Define chat (GPT-3) function
   def chat(self, question:str):
+    # Set question on valid format
     self.prompt += self.restart_sequence + question + self.start_sequence
     
+    # Get response from OpenAI
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=self.prompt,
@@ -136,17 +168,25 @@ class Rebecca_ai:
     presence_penalty=0.2,
     stop=["\n"])
     
+    # Check if response is empty
     if response.choices[0].text == "":
         answer = "Ainda não sei responder a essa pergunta, mas vou aprender com o tempo."
+    # if not, get text from response
     else:
-      answer:str = str(response.choices[0].text)    
+      answer:str = str(response.choices[0].text)
+      
+    # Show how many tokens were used    
     print("Custo em tokens: ", response.usage.total_tokens)
+    
+    # Add answer to prompt and return it
     self.prompt += answer
     return answer
 
+  # Define reset function for GPT-3
   def reset(self):
     self.prompt = self.personality
   
+  # Define log function for GPT-3
   def all_response(self, question:str):
     self.prompt += self.restart_sequence + question + self.start_sequence
     
@@ -161,11 +201,6 @@ class Rebecca_ai:
     stop=["\n"])
     
     return str(response)
-  
-  
-if __name__ == "__main__":
-  becca = Rebecca_ai()
-  print(becca.chat("o que você é becca?"))
     
     
     
